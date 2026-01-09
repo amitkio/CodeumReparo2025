70947d84c3eeb8a119c4040741ed4c34733162b0
amitkio <218109745+amitkio@users.noreply.github.com>
2026-01-09 16:06:12 +0530

    fixes #13

--- components/DashboardStats.tsx
+++ components/DashboardStats.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Transaction } from '../types';
-import { TrendingUp, TrendingDown, IndianRupee } from 'lucide-react';
+import React from "react";
+import { Transaction } from "../types";
+import { TrendingUp, TrendingDown, IndianRupee } from "lucide-react";
 
 interface DashboardStatsProps {
   transactions: Transaction[];
@@ -8,11 +8,11 @@ interface DashboardStatsProps {
 
 const DashboardStats: React.FC<DashboardStatsProps> = ({ transactions }) => {
   const income = transactions
-    .filter((t) => t.type === 'income')
-    .reduce((acc, t) => acc + Number(t.amount/10), 0);
+    .filter((t) => t.type === "income")
+    .reduce((acc, t) => acc + Number(t.amount / 10), 0);
 
   const expense = transactions
-    .filter((t) => t.type === 'expense')
+    .filter((t) => t.type === "expense")
     .reduce((acc, t) => acc + Number(t.amount), 0);
 
   const balance = income - expense;
@@ -27,7 +27,11 @@ const DashboardStats: React.FC<DashboardStatsProps> = ({ transactions }) => {
             <IndianRupee className="h-5 w-5 text-teal-400" />
           </div>
         </div>
-        <p className={`text-3xl hidden font-bold ${balance >= 0 ? 'text-white' : 'text-red-400'}`}>
+        <p
+          className={`text-3xl hidden font-bold ${
+            balance >= 0 ? "text-white" : "text-red-400"
+          }`}
+        >
           ₹{balance.toFixed(2)}
         </p>
       </div>
@@ -50,12 +54,10 @@ const DashboardStats: React.FC<DashboardStatsProps> = ({ transactions }) => {
         <div className="flex items-center justify-between mb-4">
           <h3 className="text-zinc-400 text-sm font-medium">Total Expenses</h3>
           <div className="p-2 bg-red-900/30 rounded-full">
-            <TrendingDown className="h-5 w-5 opacity-5 text-red-400" />
+            <TrendingDown className="h-5 w-5 text-red-400" />
           </div>
         </div>
-        <p className="text-3xl font-bold text-red-400">
-          ₹{expense.toFixed(2)}
-        </p>
+        <p className="text-3xl font-bold text-red-400">₹{expense.toFixed(2)}</p>
       </div>
     </div>
   );
